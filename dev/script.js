316e1e9864c7508873205c2736bda1798aaaa622
Luke Smith <larsin.218@gmail.com>
2024-05-03 15:11:24 -0600

    started on js

--- dev/script.js
+++ dev/script.js
@@ -1,7 +1,7 @@
 // Wrap all code that interacts with the DOM in a call to jQuery to ensure that
 // the code isn't run until the browser has finished rendering all the elements
 // in the html.
-$(function () {
+// $(function () {
   // TODO: Add a listener for click events on the save button. This code should
   // use the id in the containing time-block as a key to save the user input in
   // local storage. HINT: What does `this` reference in the click listener
@@ -21,6 +21,57 @@ $(function () {
   //
   // TODO: Add code to display the current date in the header of the page.
 
-  .
+  
 
+// });
+
+$(function () {
+  // Display the current date in the header of the page
+  $("#currentDay").text(dayjs().format("dddd, MMMM D"));
+
+  // Add event listener for click events on the save button
+  $(".saveBtn").on("click", function() {
+    // Get the id of the containing time-block
+    var timeBlockId = $(this).closest(".time-block").attr("id");
+    // Get the user input from the textarea within this time-block
+    var userInput = $(this).siblings(".description").val();
+    // Save the user input in local storage using the time-block id as a key
+    localStorage.setItem(timeBlockId, userInput);
+  });
+
+  // Function to update time-block classes based on current hour
+  function updateTimeBlocks() {
+    // Get the current hour using Day.js
+    var currentHour = dayjs().hour();
+
+    // Loop through each time-block
+    $(".time-block").each(function() {
+      // Get the hour from the time-block id
+      var blockHour = parseInt($(this).attr("id").split("-")[1]);
+
+      // Remove all classes (past, present, future) from the time-block
+      $(this).removeClass("past present future");
+
+      // Add past, present, or future class based on comparison with current hour
+      if (blockHour < currentHour) {
+        $(this).addClass("past");
+      } else if (blockHour === currentHour) {
+        $(this).addClass("present");
+      } else {
+        $(this).addClass("future");
+      }
+    });
+  }
+
+  // Call updateTimeBlocks to initially set time-block classes
+  updateTimeBlocks();
+
+  // Retrieve user input from local storage and set textarea values
+  $(".time-block").each(function() {
+    var timeBlockId = $(this).attr("id");
+    var storedInput = localStorage.getItem(timeBlockId);
+    if (storedInput !== null) {
+      $(this).find(".description").val(storedInput);
+    }
+  });
 });
